28e1ffa3281e17ce44633f66833156947a0b19b8
yvesd <yves.daetwyler@gmail.com>
2025-04-08 09:06:25 +0200

    Solution part B Day 1

--- src/components/Day01.js
+++ src/components/Day01.js
@@ -4,11 +4,14 @@ import Solution from './Solution';
 
 const Day01 = () => {
     const [totalDiff, setTotalDiff] = useState(0);
+    const [leftNums, setLeftNums] = useState([]);
+    const [rightNums, setRightNums] = useState([]);
+    const [similarityScore, setSimilarityScore] = useState(0);
 
     useEffect(() => {
         fetch('/resources/day01_a.txt')
             .then(response => response.text())
-            .then(data => {
+            .then(data => { 
                 const locationList = data.split('\n');
                 const left = [];
                 const right = [];
@@ -28,6 +31,9 @@ const Day01 = () => {
                 left.sort((a, b) => a - b);
                 right.sort((a, b) => a - b);
 
+                setLeftNums(left);
+                setRightNums(right);
+
                 let diff = 0;
 
                 for (let i = 0; i < left.length; i ++) {
@@ -41,10 +47,30 @@ const Day01 = () => {
             })
     }, [])
 
+    useEffect(() => {
+        if (leftNums.length === 0 || rightNums.length === 0) {
+            return;
+        }
+
+        const leftNumsArr = [...leftNums];
+        const rightNumsArr = [...rightNums];
+        let score = 0;
+
+        const countOccurences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+        leftNumsArr.forEach(leftNum => {
+            score += countOccurences(rightNumsArr, leftNum) * leftNum;
+        });
+
+        setSimilarityScore(score);
+    }, [leftNums, rightNums])
+
     return (
         <div>
             <Part partLetter="A" />
             <Solution solutionText="Total difference" solutionValue={totalDiff} />
+            <Part partLetter="B" />
+            <Solution solutionText="Similarity score" solutionValue={similarityScore} />
         </div>
     )
 }
