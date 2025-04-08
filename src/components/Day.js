1a661edce0c5281539ffe458586cd883f33c6b0d
yvesd <yves.daetwyler@gmail.com>
2025-04-08 09:08:45 +0200

    Styling adaptions

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Day = ({ dayNum }) => (
     <>
-        <h2 className="mb-4 text-3xl text-blue-400 font-Audiowide drop-shadow-[0_0_15px_rgba(59,130,246,0.9)]">Day {dayNum}</h2>
+        <h2 className="mb-5 text-3xl text-blue-400 font-Audiowide drop-shadow-[0_0_15px_rgba(59,130,246,0.9)]">Day {dayNum}</h2>
     </>
 );
 
--- src/components/Solution.js
+++ src/components/Solution.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 const Solution = ({ solutionText, solutionValue }) => (
-    <>
-         <p className="text-lg text-lime-200 font-Audiowide">
-            {solutionText}: 
-                <span className="ml-2 text-white">
-                    {solutionValue}
-                </span>
-         </p>
-    </>
+        <p className="mb-4 text-lg text-lime-200 font-Audiowide">
+        {solutionText}: 
+            <span className="ml-2 text-white">
+                {solutionValue}
+            </span>
+        </p>
 );
 
 export default Solution;
