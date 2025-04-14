ff24a575159c76d71ad23f40dcf6a81bc75df4b6
yvesd <yves.daetwyler@gmail.com>
2025-04-14 09:31:27 +0200

    small styling adaptions + Solution of Day 2 Part A

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Day from './components/Day';
 import Day01 from './components/Day01';
+import Day02 from './components/Day02';
 
 function App() {
   return (
     <div className="App w-screen h-screen bg-black mx-auto">
       <div className="pt-4 mx-auto max-w-lg flex flex-col">
-        <h1 className="mb-8 text-4xl text-green-500 text-center font-Audiowide drop-shadow-[0_0_15px_rgba(34,197,94,0.9)]">Advent of Code 2024</h1>
+        <h1 className="mb-4 text-4xl text-green-500 text-center font-Audiowide drop-shadow-[0_0_15px_rgba(34,197,94,0.9)]">Advent of Code 2024</h1>
         <Day dayNum="01" />
         <Day01 />
+        <Day dayNum="02" />
+        <Day02 />
       </div>
     </div>
   );
---
+++ src/components/Day02.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Part from "./Part";
+import Solution from "./Solution";
+
+const Day02 = () => {
+    const [reportLevels, setReportLevels] = useState([]);
+    const [safeReportLevels, setSafeReportLevels] = useState([]);
+    const [unsafeReportLevels, setUnsafeReportLevels] = useState([]);
+
+    useEffect(() => {
+        fetch('/resources/day02.txt')
+            .then(response => response.text())
+            .then(data => {
+                const reportLevelsArr = data.split('\n');
+                const reportsLevelsStructured = [];
+
+                reportLevelsArr.forEach(line => {
+                    const reportLevel = line.split(' ').map(Number);
+                    reportsLevelsStructured.push(reportLevel);
+                });
+
+                setReportLevels(reportsLevelsStructured);
+            })
+            .catch(error => {
+                console.error(error);
+           });
+    }, [])
+
+    useEffect(() => {
+        if (reportLevels.length > 0) {
+            const reportLevelsArr = [...reportLevels];
+            const safeReportLevelsArr = [];
+            const unsafeReportLevelsArr = [];
+            reportLevelsArr.forEach(reportLevel => {
+                const positiveSteps = [];
+                const stepsAmount = [];
+                for (let i = 0; i < reportLevel.length; i++) {
+                    if (i > 0) {
+                        const step = (reportLevel[i] > reportLevel[i - 1]) ? true : false;
+                        positiveSteps.push(step);
+                        stepsAmount.push(Math.abs(reportLevel[i] - reportLevel[i - 1]));
+                    }
+                }
+                if (positiveSteps.every(step => step === true) || positiveSteps.every(step => step === false)) {
+                    if (stepsAmount.every(step => [1, 2, 3].includes(step))) {
+                        safeReportLevelsArr.push(reportLevel);
+                    } else {
+                        unsafeReportLevelsArr.push(reportLevel);
+                    }   
+                } else {
+                    unsafeReportLevelsArr.push(reportLevel);
+                }
+            })
+            setSafeReportLevels(safeReportLevelsArr);
+            setUnsafeReportLevels(unsafeReportLevelsArr);
+        }
+    }, [reportLevels])
+
+    return (
+        <div>
+            <Part partLetter="A" />
+            <Solution solutionText={"Safe reports amount"} solutionValue={safeReportLevels.length} />
+        </div>
+    )
+}
+
+export default Day02;
